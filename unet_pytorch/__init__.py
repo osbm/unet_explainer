84deb5ecca2d759fc1bea8b319fe4cdcffd1cc4e
osbm <osmanfbayram@gmail.com>
2023-10-11 08:45:16 +0300

    update

--- unet_pytorch/__init__.py
+++ unet_pytorch/__init__.py
@@ -1,3 +1,3 @@
-from .dataset import ProstateDataset, get_transforms
+from .dataset import ProstateDataset
 from .utils import get_parameter_number, set_seed, plot_history, plot_overlay_4x4, plot_predictions
 from .trainer import fit_model, predict
